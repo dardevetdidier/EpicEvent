7b686a55c9b88f1d7172bb8e3a2e750049889353
Didier Dardevet <didier.dardevet@gmail.com>
2021-11-11 01:03:52 +0100

    modify settings.py
    Hide DB password

--- EpicEvent/settings.py
+++ EpicEvent/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'epic_event',
         'USER': 'ddrdv',
-        'PASSWORD': 'test1234()',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5000'
     }
