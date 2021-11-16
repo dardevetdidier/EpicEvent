e5466ac6a6948fa0e063c61f5ddae3518cb6ff6e
Didier Dardevet <didier.dardevet@gmail.com>
2021-11-16 09:58:48 +0100

    custom admin

--- EpicEvent/settings.py
+++ EpicEvent/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'EpicEvent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'EpicEvent/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
